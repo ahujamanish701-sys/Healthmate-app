624a135a75c29730a69b317f658bf55f1a81fbeb
ahujamanish701-sys <ahujamanish701@gmail.com>
2026-02-20 14:13:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,4 @@
-function showSlide(id) {
-  let slides = document.querySelectorAll(".slide");
-  slides.forEach(slide => slide.classList.remove("active"));
-  document.getElementById(id).classList.add("active");
-}
 
-function saveProfile() {
-  let name = document.getElementById("name").value;
-  let age = document.getElementById("age").value;
-  let dob = document.getElementById("dob").value;
-
-  document.getElementById("profileDisplay").innerHTML =
-    `<h3>Profile Saved!</h3>
-     Name: ${name} <br>
-     Age: ${age} <br>
-     DOB: ${dob}`;
-}
-
-function calculateBMI() {
-  let height = document.getElementById("height").value / 100;
-  let weight = document.getElementById("weight").value;
-  let bmi = (weight / (height * height)).toFixed(2);
-
-  document.getElementById("bmiResult").innerText =
-    "Your BMI is: " + bmi;
+function installApp() {
+    alert("Thank you for choosing Guava Health! Download starting...");
 }
